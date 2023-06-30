9a91d706eee5f01e0a1c537e3e9c574351d0da5a
Chetanssolanki <chetansolanki9120@gmail.com>
2023-07-01 05:14:28 +0530

    Hello

---
+++ src/lib/components/ExperienceCards.svelte
@@ -0,0 +1,46 @@
+<script>
+	import ItemCard from './ItemCard.svelte';
+</script>
+
+<div class="mb-5">
+	<ItemCard
+		timespan="June 2019"
+		href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:829f35e7-9bef-43fc-b9a3-c781f46fc71f"
+		role="Trainee Intern · L & T"
+		short_desc=""
+		external_links={[
+			{
+				label: 'Certificate',
+				url: 'https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:829f35e7-9bef-43fc-b9a3-c781f46fc71f'
+			}
+		]}
+		tags={[]}
+	>
+		<p>- Developed an online exam MCQ portal with various features</p>
+		<p>- Implemented user registration and authentication for secure access</p>
+		<p>- Created an intuitive interface for exam creation, including adding questions</p>
+		<p>- Implemented automated grading system for efficient evaluation</p>
+		<p>- Gained exposure to latest technologies and frameworks in web development</p>
+		<p>- Experienced working in a corporate environment, acquiring valuable skills and insights</p>
+	</ItemCard>
+</div>
+<div class="mb-6">
+	<ItemCard
+		timespan="July 2022"
+		href="https://www.credly.com/badges/f747a345-0c10-4ab4-9873-349f1a13ca61/public_url"
+		role="Trainee Intern · Amazon AWS"
+		short_desc=""
+		external_links={[
+			{
+				label: 'Certificate',
+				url: 'https://www.credly.com/badges/f747a345-0c10-4ab4-9873-349f1a13ca61/public_url'
+			}
+		]}
+		tags={[]}
+	>
+		<p>- Successfully completed comprehensive training program, earning a digital badge.</p>
+		<p>- Demonstrated proficiency by completing all labs and activities during the internship.</p>
+		<p>- Gained practical knowledge in various AWS services and their applications.</p>
+		<p>- Developed skills in cloud computing, infrastructure management, and security practices.</p>
+	</ItemCard>
+</div>
---
+++ src/lib/components/ProjectCards.svelte
@@ -0,0 +1,84 @@
+<script>
+	import ItemCard from './ItemCard.svelte';
+	import AnimatedLink from './atoms/AnimatedLink.svelte';
+</script>
+
+<div class="projects md:space-y-2 mt-5">
+	<ItemCard
+		timespan="April 2023"
+		href="https://github.com/Chetanssolanki/Chatbot"
+		role="MEDBOT"
+		short_desc="AI based Medical Assistant Chatbot"
+		external_links={[
+			{
+				url: 'https://chetanssolanki.github.io/HospitalLandingPage/',
+				label: 'Live Website'
+			},
+			{
+				url: 'https://drive.google.com/file/d/1x0bwDovS7acIrfPvde0LK7TOvDAvZ0Tw/view?usp=drivesdk',
+				label: 'NASA - Runner UP'
+			}
+		]}
+		tags={['HTML', 'CSS', 'JavaScript', 'MySQL', 'Python']}
+	>
+		The MEDBOT project aims to develop an AI-based Medical Assistant Chatbot with an hospital
+		website. It provides valuable assistance to doctors and patients by offering a user-friendly
+		healthcare website interface. With machine learning techniques and healthcare datasets, it
+		achieves an accuracy of 93% in understanding and responding to medical inquiries. The
+		application's robust architecture is built using the Flask framework, allowing for seamless
+		integration between the front-end and back-end components.
+	</ItemCard>
+	<ItemCard
+		timespan="February 2020"
+		href="https://ijsrd.com/Article.php?manuscript=IJSRDV7I110419"
+		role="IoT-SWMS"
+		short_desc="Smart Water Management System for Smart Cities"
+		external_links={[
+			{
+				url: 'https://ijsrd.com/Article.php?manuscript=IJSRDV7I110419',
+				label: 'Paper Published'
+			}
+		]}
+		tags={[
+			'IoT Development',
+			'Sensor Integration',
+			'Networking and Communication',
+			'HTML',
+			'CSS',
+			'JavaScript',
+			'MySQL',
+			'Data Visualization',
+			'System Integration'
+		]}
+	>
+		The IoT-based Smart Water Management System aims to address the growing need for efficient water
+		management in smart cities. By leveraging IoT technologies, the project focuses on monitoring
+		and managing water resources effectively to ensure sustainability and optimize water usage. The
+		system incorporates various sensors, communication protocols, cloud computing, and data
+		analytics to provide real-time monitoring, early leak detection, and data-driven decision-making
+		capabilities.
+	</ItemCard>
+	<!-- <ItemCard
+		timespan="January 2022 - February 2022"
+		href="https://github.com/Aunali321/torrent_scraper"
+		role="Torrent Scraper"
+		short_desc="a torrent scraper for 1337x."
+		external_links={[
+			{
+				url: 'https://github.com/Aunali321/torrent_scraper',
+				label: 'Torrent Scraper'
+			},
+			{
+				url: 'https://github.com/webtorrent/webtorrent',
+				label: 'WebTorrent'
+			}
+		]}
+		tags={['Dart', 'WebTorrent', 'Web Scraping']}
+	>
+		Scrapes 1337x torrent site to get magnet links. Magnet links are then passed to <AnimatedLink
+			href="https://github.com/webtorrent/webtorrent">WebTorrent</AnimatedLink
+		>
+		to download the file and stream it locally in VLC media player. It is built using Dart and WebTorrent.
+		It is a CLI application.
+	</ItemCard> -->
+</div>
